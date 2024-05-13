7aac1116c73feb60a2486b8ed923faed73f23295
Tiago Granelli Ribeiro <tiago.granelli@gmail.com>
2024-05-12 23:08:55 -0300

    feat: API adjustments

--- server/routes.ts
+++ server/routes.ts
@@ -1,8 +1,12 @@
 import { Router, Request, Response } from 'express';
 import axios from 'axios';
+import NodeCache from 'node-cache';
+
+const myCache = new NodeCache({ stdTTL: 100, checkperiod: 120 });
 
 const router = Router();
-const API_BASE_URL = 'https://api.mercadolibre.com/sites/MLB/search';
+const BASE_URL = 'https://api.mercadolibre.com/';
+const API_BASE_URL = `${BASE_URL}sites/MLB`;
 
 interface Item {
   id: string;
@@ -17,11 +21,56 @@ interface Item {
   free_shipping: boolean;
 }
 
-// Search items by query
+interface CategoryResponse {
+    category_id: string;
+  }
+  
+interface Category {
+name: string;
+}
+  
+async function fetchCategoryId(search: string): Promise<string> {
+    const cacheKey = `category_id_${search}`;
+    const cachedCategoryId = myCache.get<string>(cacheKey);
+    if (cachedCategoryId) {
+        return cachedCategoryId;
+    }
+    const { data } = await axios.get<CategoryResponse[]>(`${API_BASE_URL}/domain_discovery/search?limit=8&q=${encodeURIComponent(search)}`);
+    const categoryId = data[0].category_id;
+    myCache.set(cacheKey, categoryId);
+    return categoryId;
+}
+  
+
+async function fetchCategoryPath(categoryId: string): Promise<string[]> {
+    const cacheKey = `category_path_${categoryId}`;
+    const cachedCategoryPath = myCache.get<string[]>(cacheKey);
+    if (cachedCategoryPath) {
+        return cachedCategoryPath;
+    }
+    const { data: { path_from_root } } = await axios.get<{ path_from_root: Category[] }>(`${BASE_URL}/categories/${categoryId}`);
+    const categoryPath = path_from_root.map(category => category.name);
+    myCache.set(cacheKey, categoryPath);
+    return categoryPath;
+}
+
+  
+const getCategoryTree = async (search: string): Promise<string[]> => {
+    try {
+        const categoryId = await fetchCategoryId(search);
+        return await fetchCategoryPath(categoryId);
+    } catch (error) {
+        console.error("Failed to fetch category tree", error);
+        throw error;
+    }
+}
+
 router.get('/api/items', async (req: Request, res: Response) => {
     const search: string = req.query.search as string;
     try {
-        const response = await axios.get(`${API_BASE_URL}?q=${encodeURIComponent(search)}`);
+        const categories = await getCategoryTree(search)
+
+        const response = await axios.get(`${API_BASE_URL}/search?&q=${encodeURIComponent(search)}`);
         const items: Item[] = response.data.results.map((item: any) => ({
             id: item.id,
             title: item.title,
@@ -34,24 +83,30 @@ router.get('/api/items', async (req: Request, res: Response) => {
             condition: item.condition,
             free_shipping: item.shipping.free_shipping
         }));
+
         res.json({
             query: search,
-            items: items
+            categories: categories,
+            items: items,
         });
     } catch (error: any) {
         res.status(500).json({ message: "Error fetching data from MercadoLibre", error: error.message });
     }
 });
 
-// Get item details by ID
-router.get('/api/product/:id', async (req: Request, res: Response) => {
+
+router.get('/api/items/:id', async (req: Request, res: Response) => {
     const id: string = req.params.id;
     try {
-        const response = await axios.get(`https://api.mercadolibre.com/items/${id}`);
-        const item = response.data;
-        const descriptionResponse = await axios.get(`https://api.mercadolibre.com/items/${id}/description`);
+        const [itemResponse, descriptionResponse] = await Promise.all([
+            axios.get(`https://api.mercadolibre.com/items/${id}`),
+            axios.get(`https://api.mercadolibre.com/items/${id}/description`)
+        ]);
+        const item = itemResponse.data;
         const description = descriptionResponse.data;
+        const categories = await fetchCategoryPath(item.category_id)
         res.json({
+            categories: categories, 
             item: {
                 id: item.id,
                 title: item.title,
@@ -60,15 +115,16 @@ router.get('/api/product/:id', async (req: Request, res: Response) => {
                     amount: Math.floor(item.price),
                     decimals: parseInt((item.price % 1).toFixed(2).substring(2)),
                 },
-                picture_url: item.thumbnail,
+                picture_url: item.pictures[0].url,
                 condition: item.condition,
                 free_shipping: item.shipping.free_shipping,
-                sold_qty: item.sold_quantity,
+                sold_qty: item.sold_quantity, // a api do Mercado Livre não está retornando a quantidade vendida
                 description: description.plain_text
             }
         });
-    } catch (error: any) {
-        res.status(500).json({ message: "Error fetching item details", error: error.message });
+    } catch (error) {
+        console.error('Error fetching item details', error);
+        throw new Error('Failed to fetch item details');
     }
 });
 
